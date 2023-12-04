5dbff8d5aff99fc219e4ebd85f69bb93c7ae4836
chrischo360 <chrischo@bu.edu>
2023-12-04 18:07:38 -0500

    make overlaydiv cover page and add button

--- src/content/index.ts
+++ src/content/index.ts
@@ -32,15 +32,28 @@ if (article) {
 
 // add an element that covers the entire page
 const overlayDiv: HTMLDivElement = document.createElement("div");
+const button: HTMLButtonElement = document.createElement("button")
+
+// set the button's text
+button.textContent = "click me"
 
 // Set the style to cover the entire page
 overlayDiv.style.position = "fixed";
+overlayDiv.style.display = "block";
 overlayDiv.style.top = "0";
+overlayDiv.style.bottom = "0";
+overlayDiv.style.right = "0";
 overlayDiv.style.left = "0";
 overlayDiv.style.width = "100%";
 overlayDiv.style.height = "100%";
+overlayDiv.style.overflow = "hidden";
+overlayDiv.style.opacity = "1";
+overlayDiv.style.margin = "0";
+overlayDiv.style.padding = "0";
+overlayDiv.style.border = "0";
 overlayDiv.style.backgroundColor = "#FFFFFF"; // You can adjust the background color and opacity
 overlayDiv.style.zIndex = "9999999999999999"; // Set a high z-index value
 
 // Append the div to the document body
+overlayDiv.appendChild(button)
 document.body.appendChild(overlayDiv);
